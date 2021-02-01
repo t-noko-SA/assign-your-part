882fe3ce682073d6943ebf3f0cb7b209b3fad97d
t-noko-SA <saki.arai.oden@gmail.com>
2021-02-01 11:59:01 +0900

    ESLint導入

--- models/func.js
+++ models/func.js
@@ -1,11 +1,12 @@
 'use strict';
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
 
-var config = require('../config.json');
+const config = require('../config.json');
 
-function makePieceMap(req){
-
-  const memberSum = convertInputValuesIntoArray(req.cookies[config.KEY_MEMBERS]).length;
-  const pieceNameArray = convertInputValuesIntoArray(req.cookies[config.KEY_PIECES]) // => 例[ABC, DEF, GHI...] 曲ID(n0, n1, n2...) に1対1で対応
+function makePieceMap(req) {
+  const memberSum = convertReqCookieIntoArray(req.cookies[config.KEY_MEMBERS]).length;
+  const pieceNameArray = convertReqCookieIntoArray(req.cookies[config.KEY_PIECES]); // => 例[ABC, DEF, GHI...] 曲ID(n0, n1, n2...) に1対1で対応
   const inputValueArray = Object.keys(req.body).length ? Object.entries(req.body).toString().split(',') : decodeURIComponent(req.cookies[config.KEY_INPUT_VALUE_ARRAY]).split(',');
   const pieceMap = new Map(); // => {ABC: playerCountMap, DEF: playerCountMap...}
   // const pieceIdArray = inputValueArray.filter((value, idx, array) => idx % 3 === 0); // => [n0,n0,n1...] 表のpieceId の要素を昇順に格納
@@ -13,44 +14,41 @@ function makePieceMap(req){
   const playerCountArray = inputValueArray.filter((value, idx, array) => idx % 3 === 2); // => [1,1,2...] 表のplayerCount の要素を昇順に格納　(曲ID n0 のパート1の人数, 曲ID n0 のパート2の人数, 曲ID n2 のパート1の人数)
 
   pieceNameArray.forEach((pieceName, pieceNameIdx) => {
-    const playerCountMap = makePlayerCountMap(pieceNameIdx);    
+    const playerCountMap = makePlayerCountMap(pieceNameIdx);
     // console.log('playerCountMap', playerCountMap);
     const pieceInfoMap = new Map([
       [config.KEY_PIECE_NAME, pieceName],
       [config.KEY_PLAYER_COUNT, playerCountMap],
-      [config.KEY_PLAYER_SUM, sumPlayer(playerCountMap)]
+      [config.KEY_PLAYER_SUM, sumPlayer(playerCountMap)],
     ]);
 
-    pieceMap.set(pieceName, pieceInfoMap); 
+    pieceMap.set(pieceName, pieceInfoMap);
     // console.log('pieceInfoMap', pieceInfoMap)
 
-    function makePlayerCountMap(pieceNameIdx){
+    function makePlayerCountMap(pieceNameIdx) {
       const playerCountMap = new Map();
       const peaceStartIdx = memberSum * pieceNameIdx;
 
-      playerCountArray.filter((v,i) => {
-        return i >= peaceStartIdx && i <= peaceStartIdx + memberSum - 1;
-      }).forEach((v,i) => {
-        playerCountMap.set( i + 1 , v);
-      })
+      playerCountArray.filter((v, i) => i >= peaceStartIdx && i <= peaceStartIdx + memberSum - 1).forEach((v, i) => {
+        playerCountMap.set(i + 1, v);
+      });
       return playerCountMap;
     }
-  });   
+  });
 
   pieceNameArray.forEach((pieceName) => {
-    // console.log(pieceName, pieceMap.get(pieceName).get(config.KEY_PLAYER_COUNT));
-    }
-  );
-  
-  return [pieceMap, inputValueArray];  
-};
+    console.log(pieceName, pieceMap.get(pieceName).get(config.KEY_PLAYER_COUNT));
+  });
 
-function assignParts(pieceMap, memberArray){
+  return [pieceMap, inputValueArray];
+}
+
+function assignParts(pieceMap, memberArray) {
   const memberMap = makeMemberMap(pieceMap, memberArray);
-  const unAssignedMemberMap = makeUnAssignedMemberMap(memberArray, memberMap);  
+  const unAssignedMemberMap = makeUnAssignedMemberMap(memberArray, memberMap);
   const sortedPieceArray = makeSortedPieceArray(pieceMap);
 
-  sortedPieceArray.forEach(pieceName =>{
+  sortedPieceArray.forEach((pieceName) =>{
     const piece = pieceMap.get(pieceName);
     const playerArray = makePlayerArray(piece, memberMap, unAssignedMemberMap);
     const playerCountMap = piece.get(config.KEY_PLAYER_COUNT);
@@ -59,152 +57,155 @@ function assignParts(pieceMap, memberArray){
     const assigntmentMap = makeAssigntmentMap(playerArray, playerCountMap);
     // console.log('assigntmentMap', assigntmentMap);
     piece.set(config.KEY_ASSIGNTMENT_MAP, assigntmentMap);
-    });
+  });
   return pieceMap;
-};
+}
 
-function makeMemberMap(pieceMap, memberArray){
-  const memberSum = memberArray.length;  
+function makeMemberMap(pieceMap, memberArray) {
+  const memberSum = memberArray.length;
   const playerTotal = countPlayerTotal(pieceMap);
   const memberMap = new Map();
   const extraPlayerArray = chooseExtraPlayer(memberArray, memberSum, playerTotal);
 
-  memberArray.forEach(memberName => {
+  memberArray.forEach((memberName) => {
     const memberInfoMap = makeMemberInfoMap(memberName, memberSum, playerTotal, extraPlayerArray);
     memberMap.set(memberName, memberInfoMap);
   });
   return memberMap;
 }
 
-function makeMemberInfoMap(memberName, memberSum, playerTotal, extraPlayerArray){
+function makeMemberInfoMap(memberName, memberSum, playerTotal, extraPlayerArray) {
   const memberInfoMap = new Map();
   const playingPieceSum = extraPlayerArray.includes(memberName) ? Math.floor(playerTotal / memberSum) + 1 : Math.floor(playerTotal / memberSum);
-  memberInfoMap.set(config.KEY_PLAYER_NAME, memberName);  
+  memberInfoMap.set(config.KEY_PLAYER_NAME, memberName);
   memberInfoMap.set(config.KEY_PLYAING_PIECE_SUM, playingPieceSum);
 
   return memberInfoMap;
 }
 
-function makeUnAssignedMemberMap(memberArray, memberMap){
-  const unAssignedMemberMap = new Map;
-  memberArray.forEach(memberName => {
+function makeUnAssignedMemberMap(memberArray, memberMap) {
+  const unAssignedMemberMap = new Map();
+  memberArray.forEach((memberName) => {
     unAssignedMemberMap.set(memberName, memberMap.get(memberName).get(config.KEY_PLYAING_PIECE_SUM));
   });
   return unAssignedMemberMap;
 }
 
-function chooseExtraPlayer(memberArray, memberSum, playerTotal){
+function chooseExtraPlayer(memberArray, memberSum, playerTotal) {
   const extraPlayerArray = [];
   const extraPlayerSum = playerTotal % memberSum;
   const coppiedMemberArray = Array.from(memberArray);
-  
-  if(extraPlayerSum === 0) {
-    return extraPlayerArray;    
-  } else {
-    let i = extraPlayerSum;
-    while (i > 0) {
-      const choosedIndex = chooseIndex(coppiedMemberArray.length);
-      extraPlayerArray.push(coppiedMemberArray[choosedIndex]);
-      coppiedMemberArray.splice(choosedIndex, 1);
-      i--;
-    }
-  }  
+
+  if (extraPlayerSum === 0) {
+    return extraPlayerArray;
+  }
+  let i = extraPlayerSum;
+  while (i > 0) {
+    const choosedIndex = chooseIndex(coppiedMemberArray.length);
+    extraPlayerArray.push(coppiedMemberArray[choosedIndex]);
+    coppiedMemberArray.splice(choosedIndex, 1);
+    i -= 1;
+  }
+
   // console.log('extraPlayerArray', extraPlayerArray);
   return extraPlayerArray;
 }
 
-function chooseIndex(arrayLength){
-  const choosedIndex = Math.floor( Math.random() * arrayLength);
+function chooseIndex(arrayLength) {
+  const choosedIndex = Math.floor(Math.random() * arrayLength);
   return choosedIndex;
 }
 
-function makePlayerArray(piece, memberMap, unAssignedMemberMap){
+function makePlayerArray(piece, memberMap, unAssignedMemberMap) {
   const playerArray = [];
-  const unAssignedMemberArray = Array.from(unAssignedMemberMap.keys());//TODO debug
+  const unAssignedMemberArray = Array.from(unAssignedMemberMap.keys());
   // console.log('unAssignedMemberArray',unAssignedMemberArray);
-  const playerSum = piece.get(config.KEY_PLAYER_SUM);//OK
+  const playerSum = piece.get(config.KEY_PLAYER_SUM);
   let i = playerSum;
   while (i > 0) {
     const choosedIndex = chooseIndex(unAssignedMemberArray.length);
     const choosedMember = unAssignedMemberArray[choosedIndex];
     playerArray.push(choosedMember);
-    unAssignedMemberArray.splice(unAssignedMemberArray.indexOf(choosedMember),1);//TODO 同名がいたときに不具合が出る
+    unAssignedMemberArray.splice(unAssignedMemberArray.indexOf(choosedMember), 1);
     // console.log('choosedMember', choosedMember, 'unAssignedMemberArray', unAssignedMemberArray);
-    unAssignedMemberMap.set(choosedMember, Number(unAssignedMemberMap.get(choosedMember))-1);
-    if (unAssignedMemberMap.get(choosedMember)===0) {
+    unAssignedMemberMap.set(choosedMember, Number(unAssignedMemberMap.get(choosedMember)) - 1);
+    if (unAssignedMemberMap.get(choosedMember) === 0) {
       unAssignedMemberMap.delete(choosedMember);
       // console.log('deleted', choosedMember, 'unAssignedMemberMap', unAssignedMemberMap);
     }
     // console.log('unAssignedMemberMap', unAssignedMemberMap);
     i--;
   }
-  
+
   // console.log('pieceName', piece.get(config.KEY_PIECE_NAME),'playerArray', playerArray, 'unAssignedMemberArray', unAssignedMemberArray)// playerArray.push()
   return playerArray;
 }
 
-function makeAssigntmentMap(playerArray, playerCountMap){
-  const assigntmentMap = new Map;
+function makeAssigntmentMap(playerArray, playerCountMap) {
+  const assigntmentMap = new Map();
   let playerArrayIdx = 0;
 
-  playerCountMap.forEach((playerCount,part,array) =>{
+  playerCountMap.forEach((playerCount, part, array) =>{
     const partPlayerArray = [];
-    for (let i = playerCount; i > 0; i --){
+    for (let i = playerCount; i > 0; i--) {
       // console.log('playerArray[playerArrayIdx]', playerArray[playerArrayIdx]);
       partPlayerArray.push(playerArray[playerArrayIdx]);
-      playerArrayIdx++;
-    };
+      playerArrayIdx += 1;
+    }
     if (partPlayerArray.length > 0) {
       assigntmentMap.set(part, partPlayerArray);
-    };
-  })
+    }
+  });
   return assigntmentMap;
 }
 
-function sumPlayer (playerCountMap) {
+function sumPlayer(playerCountMap) {
   let playerSum = 0;
   // console.log('playerCountMap', playerCountMap);
-  playerCountMap.forEach(playerCount => {
-    playerSum = playerSum + Number(playerCount);
-  });  
+  playerCountMap.forEach((playerCount) => {
+    playerSum += Number(playerCount);
+  });
   return playerSum;
-};
+}
 
-function countPlayerTotal (pieceMap) {
+function countPlayerTotal(pieceMap) {
   let playerTotal = 0;
-  pieceMap.forEach(piece => {
-    playerTotal = playerTotal + sumPlayer (piece.get(config.KEY_PLAYER_COUNT));
+  pieceMap.forEach((piece) => {
+    playerTotal += sumPlayer(piece.get(config.KEY_PLAYER_COUNT));
   });
   return playerTotal;
-};
+}
 
-function makeSortedPieceArray (pieceMap) {
-  const pieceArray = Array.from(pieceMap.keys()); 
-  let tempArray = pieceArray.map((pieceName) => (
-    {key: pieceName, value: pieceMap.get(pieceName).get(config.KEY_PLAYER_SUM)}
+function makeSortedPieceArray(pieceMap) {
+  const pieceArray = Array.from(pieceMap.keys());
+  const tempArray = pieceArray.map((pieceName) => (
+    { key: pieceName, value: pieceMap.get(pieceName).get(config.KEY_PLAYER_SUM) }
   ));
   const sortedPieceArray = tempArray.sort((a, b) => {
-    if(a.value < b.value) return 1;
-    if(a.value > b.value) return -1;
-    if(a.value = b.value) return Math.floor(Math.random() * 3) -1;// -> -1 or 0 or 1
-  }).map(v => v.key);
+    if (a.value < b.value) return 1;
+    if (a.value > b.value) return -1;
+    if (a.value === b.value) return Math.floor(Math.random() * 3) - 1;// -> -1 or 0 or 1
+  }).map((v) => v.key);
   // console.log('tempArray', tempArray);
   // console.log('sortedPieceArray', sortedPieceArray);
   return sortedPieceArray;
-};
+}
 
-function convertInputValuesIntoArray(inputValues) {
+function convertReqCookieIntoArray(inputValues) {
   // const arr = decodeURIComponent(inputValuesOrReqCookies).trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
-  const arr = decodeURIComponent(inputValues).slice(0, config.PARAM_MAX_INPUT_SIZE).trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
+  // const arr = decodeURIComponent(inputValues).slice(0, config.PARAM_MAX_INPUT_SIZE).trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
+  const arr = decodeURIComponent(inputValues).slice(0, config.PARAM_MAX_INPUT_SIZE).trim().split('\n')
+    .map((s) => s.trim())
+    .filter((s) => s !== '');
 
-  if(arr.length>config.PARAM_MAX_INPUT_LENGTH){
-    arr.length=config.PARAM_MAX_INPUT_LENGTH;
-  };
-  return Array.from(new Set(arr));//重複排除
-};
+  if (arr.length > config.PARAM_MAX_INPUT_LENGTH) {
+    arr.length = config.PARAM_MAX_INPUT_LENGTH;
+  }
+  return Array.from(new Set(arr));
+}
 
-function formatReqCookie(reqCookie){ //cookie -> req.cookies['members']
-  return decodeURIComponent(reqCookie).slice(0,config.PARAM_MAX_INPUT_SIZE);
-};
+function formatReqCookie(reqCookie) { //cookie -> req.cookies['members']
+  return decodeURIComponent(reqCookie).slice(0, config.PARAM_MAX_INPUT_SIZE);
+}
 
-module.exports = {convertInputValuesIntoArray, makePieceMap, assignParts, formatReqCookie};
+module.exports = { convertReqCookieIntoArray, makePieceMap, assignParts, formatReqCookie };
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,12 @@
 'use strict';
-//TODO 変数名と値が全体で一致するようにする
+// TODO 変数名と値が全体で一致するようにする
 const config = require('../config.json');
 const express = require('express');
-const {convertInputValuesIntoArray,makePieceMap, assignParts, formatReqCookie} = require('../models/func');
+const {
+  convertReqCookieIntoArray, makePieceMap, assignParts, formatReqCookie,
+} = require('../models/func');
 const isLocalhost = require('../models/isLocalhost');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -14,85 +17,84 @@ router.post('/', (req, res, next) => {
   res.render('form1');
 });
 
-router.get('/form/1', (req, res, next) => {  
-  console.log('members',formatReqCookie(req.cookies[config.KEY_MEMBERS]));
-  res.render('form1',{
-    members : req.cookies[config.KEY_MEMBERS]? formatReqCookie(req.cookies[config.KEY_MEMBERS]):"",
-    pieces : req.cookies[config.KEY_PIECES]? formatReqCookie(req.cookies[config.KEY_PIECES]):""
+router.get('/form/1', (req, res, next) => {
+  res.render('form1', {
+    members: req.cookies[config.KEY_MEMBERS] ? formatReqCookie(req.cookies[config.KEY_MEMBERS]) : '',
+    pieces: req.cookies[config.KEY_PIECES] ? formatReqCookie(req.cookies[config.KEY_PIECES]) : '',
   });
 });
 
 router.post('/form/1', (req, res, next) => {
-  const members = req.body[config.KEY_MEMBERS].slice(0, config.PARAM_MAX_INPUT_SIZE);//TODO 重複メンバーは除外する(mapのキーになる為)
-  const pieces = req.body[config.KEY_PIECES].slice(0, config.PARAM_MAX_INPUT_SIZE);
-  console.log('members', members);
-  console.log('pieces', pieces);
+  const members = encodeURIComponent(req.body[config.KEY_MEMBERS])
+    .slice(0, config.PARAM_MAX_INPUT_SIZE);
+  const pieces = encodeURIComponent(req.body[config.KEY_PIECES])
+    .slice(0, config.PARAM_MAX_INPUT_SIZE);
   const location = typeof window === 'object' ? window.location : {};
-  const host = typeof location === 'object' ? location.host : '';    
+  const host = typeof location === 'object' ? location.host : '';
 
-  if(!isLocalhost(host)){
-    res.cookie(config.KEY_MEMBERS,encodeURIComponent(members),{      
-      secure: true, httpOnly: true, sameSite: true
+  if (!isLocalhost(host)) {
+    res.cookie(config.KEY_MEMBERS, members, {
+      secure: true, httpOnly: true, sameSite: true,
     });
-    res.cookie(config.KEY_PIECES,encodeURIComponent(pieces),{      
-      secure: true, httpOnly: true, sameSite: true
-    });    
-  }else{
-    res.cookie(config.KEY_MEMBERS,encodeURIComponent(members),{      
-      httpOnly: true, sameSite: true
+    res.cookie(config.KEY_PIECES, pieces, {
+      secure: true, httpOnly: true, sameSite: true,
     });
-    res.cookie(config.KEY_PIECES,encodeURIComponent(pieces),{
-      httpOnly: true, sameSite: true
+  } else {
+    res.cookie(config.KEY_MEMBERS, members, {
+      httpOnly: true, sameSite: true,
+    });
+    res.cookie(config.KEY_PIECES, pieces, {
+      httpOnly: true, sameSite: true,
     });
   }
   res.redirect('/form/2');
-  });
+});
 
-    
-
-router.post('/form/1:clear',(req, res, next) =>{
+router.post('/form/1:clear', (req, res, next) =>{
   res.clearCookie(config.KEY_MEMBERS);
   res.clearCookie(config.KEY_PIECES);
   res.redirect('/form/1');
-})
+});
 
 router.get('/form/2', (req, res, next) => {
-  console.log('memberCnt',convertInputValuesIntoArray(req.cookies[config.KEY_MEMBERS]).length, 'pieceCnt', convertInputValuesIntoArray(req.cookies[config.KEY_PIECES]).length)
   res.render('form2', {
-    memberCnt : convertInputValuesIntoArray(req.cookies[config.KEY_MEMBERS]).length, 
-    pieceCnt : convertInputValuesIntoArray(req.cookies[config.KEY_PIECES]).length,
-    pieces : convertInputValuesIntoArray(req.cookies[config.KEY_PIECES])
-  });
+    // memberCnt: convertReqCookieIntoArray(req.cookies[config.KEY_MEMBERS]).length,
+    // pieceCnt: convertReqCookieIntoArray(req.cookies[config.KEY_PIECES]).length,
+    // pieces: convertReqCookieIntoArray(req.cookies[config.KEY_PIECES]),
+    memberCnt: convertReqCookieIntoArray(req.cookies[config.KEY_MEMBERS]).length,
+    pieceCnt: convertReqCookieIntoArray(req.cookies[config.KEY_PIECES]).length,
+    pieces: convertReqCookieIntoArray(req.cookies[config.KEY_PIECES]),
 
+  });
 });
 
 router.post('/form/2', (req, res, next) => {
-  const memberArray = convertInputValuesIntoArray(req.cookies[config.KEY_MEMBERS]);
+  const memberArray = convertReqCookieIntoArray(req.cookies[config.KEY_MEMBERS]);
   const pieceMap = makePieceMap(req)[0];
   const resultPieceMap = assignParts(pieceMap, memberArray);//TODO modelsに移す
   const location = typeof window === 'object' ? window.location : {};
-  const host = typeof location === 'object' ? location.host : '';      
+  const host = typeof location === 'object' ? location.host : '';
   console.log('pieceMap', pieceMap);
   // console.log('resultPieceMap', resultPieceMap);
   // console.log('pieces', req.body[config.KEY_PIECES].slice(0, config.PARAM_MAX_INPUT_SIZE));
-  if(!isLocalhost(host)){
-    res.cookie(config.KEY_INPUT_VALUE_ARRAY, encodeURIComponent(makePieceMap(req)[1].toString()),{
-      secure: true, httpOnly: true, sameSite: true
+  if (!isLocalhost(host)) {
+    res.cookie(config.KEY_INPUT_VALUE_ARRAY, encodeURIComponent(makePieceMap(req)[1].toString()), {
+      secure: true, httpOnly: true, sameSite: true,
     });
-  }else{
-    res.cookie(config.KEY_INPUT_VALUE_ARRAY, encodeURIComponent(makePieceMap(req)[1].toString()),{
-      httpOnly: true, sameSite: true
-    });  
+  } else {
+    res.cookie(config.KEY_INPUT_VALUE_ARRAY, encodeURIComponent(makePieceMap(req)[1].toString()), {
+      httpOnly: true, sameSite: true,
+    });
   }
-  res.render('result',{
-    pieces : convertInputValuesIntoArray(req.cookies[config.KEY_PIECES].slice(0,config.PARAM_MAX_INPUT_SIZE)),
-    result : resultPieceMap,
-    assigntmenMapKey : config.KEY_ASSIGNTMENT_MAP
+  res.render('result', {
+    pieces: convertReqCookieIntoArray(req.cookies[config.KEY_PIECES]),
+    result: resultPieceMap,
+    assigntmenMapKey: config.KEY_ASSIGNTMENT_MAP,
   });
 });
 
 router.post('/retry', (req, res, next) => {
   res.redirect(req.get('/form/2'));
-})
+});
 
 module.exports = router;
